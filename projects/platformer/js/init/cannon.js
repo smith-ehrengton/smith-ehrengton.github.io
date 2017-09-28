02d8fdc2e94b0da950e8c6ede4d4d5e168dc3111
Ehrengton Smith <ehrengton.smith@gmail.com>
2017-09-28 00:30:28 +0000

    add portfolio.html file

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,24 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary to make your level challenging.
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(450);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
